2df8b2b0b597d9a835aa3b62639a254fb95cf884
Ahmad Iqbal Firmansyah <ahmedeqbal101@gmail.com>
2024-09-20 08:32:28 +0700

    Jobsheet 4 - Praktikum 2.4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,10 +7,11 @@
     <h1>Data user</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            <td>ID</td>
+            {{-- <td>ID</td>
             <td>Username</td>
             <td>Nama</td>
-            <td>ID Level pengguna</td>
+            <td>ID Level pengguna</td> --}}
+            <td>Jumlah pengguna</td>
         </tr>
         {{-- @foreach ($data as $d) --}}
             <tr>
@@ -19,10 +20,13 @@
                 <td> {{$d->nama}} </td>
                 <td> {{$d->level_id}} </td> --}}
 
-                <td>{{ $data->user_id}}</td>
+                {{-- <td>{{ $data->user_id}}</td>
                 <td>{{ $data->username}}</td>
                 <td>{{ $data->nama}}</td>
-                <td>{{ $data->level_id}}</td>
+                <td>{{ $data->level_id}}</td> --}}
+                <td>{{ $userCount}}</td> 
+
+
             </tr>
         {{-- @endforeach --}}
     </table>
