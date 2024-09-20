06f5a5aeb5c29136e061a9b93dc77e2776e802ef
Ahmad Iqbal Firmansyah <ahmedeqbal101@gmail.com>
2024-09-20 08:06:01 +0700

    Jobsheet 4 - Praktikum 2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -25,14 +25,14 @@ class UserController extends Controller
         //     'password' => Hash::make('12345')
         // ];
 
-        $data =[
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
+        // $data =[
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
 
-        UserModel::create($data);
+        // UserModel::create($data);
         //tambah data dengan Eloquent
         // $data = [
         //     'nama' => 'Pelanggan pertama',
@@ -40,7 +40,19 @@ class UserController extends Controller
         // UserModel::where('username', 'customer 1')->update($data); //update data user
 
         // coba akses model UserModel
-        $user = UserModel::all(); //ambil semua data m_user
+        // $user = UserModel::all(); //ambil semua data m_user
+        
+        // $user = UserModel::find(1);
+
+        // $user = UserModel::where('level_id', 1)->first();
+
+        // $user = UserModel::firstWhere('level_id',1);
+
+        // $user = UserModel::findOr(1,['username', 'nama'], function (){
+        
+        $user = UserModel::findOr(20,['username', 'nama'], function (){
+        abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
