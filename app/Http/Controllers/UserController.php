589cbd9c1e22419d808e70fb9bb582f854bb8619
Ahmad Iqbal Firmansyah <ahmedeqbal101@gmail.com>
2024-09-27 09:40:38 +0700

    Jobsheet 5 - Praktikum 4

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -23,7 +23,9 @@ class UserController extends Controller
 
     $activeMenu = 'user'; //set menu yang sedang aktif
 
-    return view('user.index',['breadcrumb'=>$breadcrumb, 'page' => $page, 'activeMenu'=>$activeMenu]);
+    $level = LevelModel::all();// ambil data level untuk filter level
+
+    return view('user.index',['breadcrumb'=>$breadcrumb, 'page' => $page,'level' => $level, 'activeMenu'=>$activeMenu]);
     }
     //Ambil data user dalam bentuk json untuk datables
     public function list(Request $request)
@@ -31,6 +33,7 @@ class UserController extends Controller
         $users = UserModel::select('user_id', 'username', 'nama', 'level_id')
             ->with('level');
 
+        //Filter data user berdasarkan level_id
         if ($request->level_id) {
             $users->where('level_id', $request->level_id);
         }
